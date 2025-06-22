064463a1eef27f71613191c2a7a383ff546669bd
Kousei-Uchu <kouseicomms@gmail.com>
2025-06-23 09:56:05 +1000

    a

--- components/HomeContent.js
+++ components/HomeContent.js
@@ -7,7 +7,7 @@ export default function HomeContent() {
   const [expandedGroup, setExpandedGroup] = useState(null);
 
   useEffect(() => {
-    fetch('/affirmation-storage/affirmation.json')
+    fetch('affirmation-storage/affirmation.json')
       .then(response => {
         if (!response.ok) throw new Error("Failed to load affirmation");
         return response.json();
