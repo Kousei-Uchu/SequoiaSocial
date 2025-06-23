fca7fec2e650ab8e897ff4d6fd49a387566ac300
Kousei-Uchu <kouseicomms@gmail.com>
2025-06-23 12:56:47 +1000

    a

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
         {/* Font Awesome CDN */}
         <link 
           rel="stylesheet" 
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" 
           integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" 
           crossOrigin="anonymous" 
           referrerPolicy="no-referrer" 
