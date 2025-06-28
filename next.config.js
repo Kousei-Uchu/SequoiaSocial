0f552553e28756fa6ff19c3d5ba4fded5b6bf2d7
Kousei Uchu <kouseicomms@gmail.com>
2025-06-28 20:26:01 +1000

    a

--- next.config.js
+++ next.config.js
@@ -1,33 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // WASM support for crypto-js
+  webpack: (config) => {
     config.experiments = {
       asyncWebAssembly: true,
-      layers: true,
-      topLevelAwait: true
+      layers: true
     };
-
-    // Explicit WASM loader rule
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: 'webassembly/async',
-    });
-
-    // Important: Disable WASM optimization that causes issues
-    config.optimization = {
-      ...config.optimization,
-      minimize: false
-    };
-
     return config;
   },
-
-  // Required for crypto package
-  transpilePackages: [
-    '@matrix-org/matrix-sdk-crypto-js',
-    '@matrix-org/matrix-sdk-crypto-wasm'
-  ],
+  transpilePackages: ['@matrix-org/matrix-sdk-crypto']
 }
 
 module.exports = nextConfig
